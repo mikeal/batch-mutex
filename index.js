b72cb3da717969c80a032149b61d559a57142b25
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-12-03 23:34:18 +0000

    init: simple batch mutex

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = (batch, maxQueue = 1000) => {
+  let waiting
+  let queue = []
+  let onEmpties = []
+
+  const flush = async () => {
+    const writes = queue
+    queue = []
+    waiting = batch(writes)
+    await waiting
+    waiting = null
+    if (queue.length) flush()
+    else {
+      onEmpties.forEach(e => e())
+      onEmpties = []
+    }
+  }
+
+  const write = (...args) => {
+    queue.push(args)
+    if (!waiting) flush()
+    else if (queue.length > maxQueue) {
+      return waiting
+    }
+  }
+  write.onEmpty = () => {
+    return new Promise(resolve => {
+      onEmpties.push(resolve)
+    })
+  }
+  return write
+}
---
+++ test/test-basics.js
@@ -0,0 +1,51 @@
+const assert = require('assert')
+const main = require('../')
+
+const sum = (x, y) => x + y
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const basicTest = async max => {
+  let init = true
+  let final = false
+  const totals = []
+  let goal = 0
+  let allWrites = 0
+  const batch = async writes => {
+    const total = writes.map(o => o[0]).reduce(sum)
+    totals.push(total)
+    allWrites += writes.length
+    await sleep(100)
+    if (init) {
+      init = false
+    } else {
+      if (final) return
+      final = true
+      let i = 0
+      while (i < 1000) {
+        write(i)
+        i++
+        goal++
+      }
+    }
+  }
+  const write = main(batch, max)
+  let i = 0
+  while (i < 500) {
+    i++
+    goal++
+    await write(i)
+  }
+  await write.onEmpty()
+  assert.strictEqual(goal, allWrites)
+  return totals
+}
+
+exports.testBatch = async () => {
+  const totals = await basicTest(100)
+  assert.deepStrictEqual(totals, [1, 5252, 514953, 25654, 35855, 43035])
+}
+exports.testBatchDefault = async () => {
+  const totals = await basicTest()
+  assert.deepStrictEqual(totals, [1, 125249, 499500])
+}
